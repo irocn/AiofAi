2dfd6ffa34302edbf5e1d23456d0441ef19f9d74
hao.chen <hao.chen@irocn.com>
2024-03-18 04:54:52 +0000

    sidebar and inport ui improved

--- gptui/src/app/sidebar/sidebar.component.ts
+++ gptui/src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgStyle],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
